7139bc82ee25f988006e3ecb3e33df320ddf178e
Elysia <117100940+aykkdk@users.noreply.github.com>
2025-01-29 01:38:45 +0800

    Add files via upload

---
+++ GQ2.c
@@ -0,0 +1,10 @@
+#include <stdio.h>		
+#include <stdlib.h>	
+
+int main(void) {		
+    printf("¯Z¯Å:?????\n"); 
+    printf("¾Ç¸¹:********\n"); 
+    printf("©m¦W:$$$\n"); 
+    system("pause");	
+    return 0;
+}
